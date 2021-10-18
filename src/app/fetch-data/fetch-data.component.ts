0b3a4ca340ace9b0b2edebf1533ef01bec6fdc82
suhayb-maudarbocus <s.maudar6@gmail.com>
2021-10-18 16:39:33 +0400

    first commit

---
+++ src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent {
+  public forecasts: WeatherForecast[];
+  public Employee: Employee[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+
+    http.get<Employee[]>(baseUrl + 'employees').subscribe(result => {
+      this.Employee = result;
+      console.log("employee:" , result);
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+interface Employee{
+  EmployeeId: number,
+  EmployeeEmail: string,
+  EmployeePassword: string,
+  EmplpoyeeRole: string
+}
